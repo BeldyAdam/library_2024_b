e69674870f811c59ee74af1a554e8017da112ab9
BeldyAdam <beldy.adam@gmail.com>
2024-09-24 12:08:51 +0200

    Books

--- konyvtar/database/seeders/DatabaseSeeder.php
+++ konyvtar/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+/*         User::factory(10)->create();
+        
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]); */
+        
+        Book::factory(10)->create();
+
+        Book::factory()->create([
+            'author' => 'Test Name',
+            'title' => 'Test Title',
+            'pieces' => 10 //integerValue()
         ]);
     }
 }
---
+++ konyvtar/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+Route::post('/user', [UserController::class, 'store']);
+
+Route::get('books', [BookController::class, 'index']);
